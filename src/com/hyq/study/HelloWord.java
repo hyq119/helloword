a173e2906444d1c330f1bf003f5b530f0828afe3
hyq119 <hyq119@qq.com>
2017-01-17 21:50:08 +0800

    自己在家摸索

---
+++ src/com/hyq/study/HelloWord.java
@@ -0,0 +1,12 @@
+package com.hyq.study;
+
+import java.util.Date;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWord " + new Date().toLocaleString());
+	}
+
+}
