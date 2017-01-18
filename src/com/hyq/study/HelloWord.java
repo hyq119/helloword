8d27f8d738c61be28e22654b1fd01dcb1a66a82c
hyq119 <hyq119@qq.com>
2017-01-18 15:57:50 +0800

    patch

--- src/com/hyq/study/HelloWord.java
+++ src/com/hyq/study/HelloWord.java
@@ -6,6 +6,6 @@ public class HelloWord {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWord " + new Date().toGMTString());
+		System.out.println("HelloWord patch" + new Date().toGMTString());
 	}
 }
